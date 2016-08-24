2c1fd4141582614e10f6b95c11102d79682f5d4c
acsauk <acsauk@gmail.com>
2016-08-24 16:21:06 +0100

    failing tests for associations between models

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'devise'
 
 gem 'omniauth-facebook'
 
+gem 'shoulda-matchers'
+
 
 group :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,17 +6,27 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'shoulda-matchers'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
-  
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
 
 
   config.infer_spec_type_from_file_location!
-
 
 end
